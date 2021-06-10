46c27061fddfea78f57086673fc2f9eb7bd1a163
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-10 17:33:23 +1200

    Fix CLI case-sensitivity

--- changelog.md
+++ changelog.md
@@ -1,24 +1,18 @@
 # Changelog
 
-- [Versions](#versions)
-- [Data updates](#data-updates)
+Build number is two-digit year + two-digit week.
 
-## Versions
+## 0.1.3
+- [CLI] Fixed input case sensitivity.
 
-### 0.1.2
+## 0.1.2
 - [API] Fixed import and export not being specified correctly.
 
-### 0.1.1
+## 0.1.1
 - [CLI] Changed input to be case-insensitive.
 - [CLI] Fixed command-line usage not working
 
-### 0.1.0
+## 0.1.0
 - [API] Added API for retrieving versions.
 - [CLI] Added command-line interface.
-
-## Data updates
-
-Build number is two-digit year + two-digit week.
-
-### build 2122
-- Added Java Release versions.
+- [Data] Added Java Release versions.
--- src/cli.ts
+++ src/cli.ts
@@ -5,16 +5,17 @@ import versions from './index.js';
 import { EditionTable, PhaseTable, VersionTable, Version } from './types.js';
 
 function log() {
+    const caps = (text: string): string => text[0].toUpperCase() + text.substr(1).toLowerCase();
     const args: string[] = process.argv.slice(2);
     let [edition, phase, version, n]: string[] = args;
 
     let editionTable: EditionTable = versions, phaseTable: PhaseTable, versionTable: VersionTable, versionList: Version[];
     if (!edition) return console.log(editionTable);
-    phaseTable = editionTable[edition.toLowerCase()];
+    phaseTable = editionTable[caps(edition)];
     if (!phase) return console.log(phaseTable);
-    versionTable = phaseTable[phase.toLowerCase()];
+    versionTable = phaseTable[caps(phase)];
     if (!version) return console.log(versionTable);
-    versionList = versionTable[version.toLowerCase()];
+    versionList = versionTable[version];
     if (!n) return console.log(versionList);
     return console.log(versionList[+n]);
 }
