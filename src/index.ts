a83462675137d9b70408a51d9ea16bc1456666b6
Nixinova <nixinova@gmail.com>
2021-08-21 17:11:44 +1200

    Split version data file per-edition

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,12 @@ import { VersionData, EditionTable, PhaseTable } from './types.js';
 
 const Versions = {} as EditionTable;
 
-const filename = path.resolve(__dirname, '../src/versions.yml');
-const fileData: string = fs.readFileSync(filename, { encoding: 'utf8' });
-const versionData = yaml.load(fileData) as VersionData[];
+const versionData: VersionData[] = [];
+for (const file of ['java', 'bedrock']) {
+    const filePath = path.resolve(__dirname, `../data/${file}.yaml`);
+    const fileData: string = fs.readFileSync(filePath, { encoding: 'utf8' });
+    versionData.push(...<VersionData[]>yaml.load(fileData));
+}
 
 for (const data of versionData) {
     const [id, edition, phase, parent, vers] = data;
